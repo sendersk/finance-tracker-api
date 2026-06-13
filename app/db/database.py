bac289fdb3c6fc6fc4b07cdb00a3a0957442cc5d
psend <psenderski94@gmail.com>
2026-06-13 10:17:54 +0200

    feat: add SQLAlchemy declarative base

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 from app.core.config import DATABASE_URL
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 engine = create_engine(
     DATABASE_URL,
