aebc4d68fe8984d28b9fb7cc817857f7ccd4c032
psend <psenderski94@gmail.com>
2026-06-13 10:22:56 +0200

    feat: create transaction model

---
+++ app/models/transaction.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from sqlalchemy import DateTime, Float, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    titile: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+
+    amount: Mapped[float] = mapped_column(
+        Float,
+        nullable=False
+    )
+
+    category: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow()
+    )
