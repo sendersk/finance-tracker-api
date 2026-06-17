facb5b0d1ccf8d9efb279131a8283d97650d1bf8
psend <psenderski94@gmail.com>
2026-06-17 15:13:43 +0200

    refactor: remove automatic schema creation

--- app/main.py
+++ app/main.py
@@ -2,13 +2,10 @@ from fastapi import FastAPI
 
 from app.core.config import settings
 from app.core.logger import setup_logger
-from app.db.database import create_tables
 from app.routes.health import router as health_router
 from app.routes.transactions import router as transaction_router
 
 setup_logger()
-
-create_tables()
 
 app = FastAPI(title=settings.APP_NAME)
 
