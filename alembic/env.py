f0df662cf6349a2016cff0c971e3b0b590f69d6f
psend <psenderski94@gmail.com>
2026-06-17 15:11:14 +0200

    config: register metadata for alembic autogenerate

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import app.models
 from app.core.config import settings
+from app.db.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
